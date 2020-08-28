59180afc1d338f9b5b1cca92f46edc3f7fff4fe9
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2020-08-28 02:06:15 +0000

    added the user management page

---
+++ src/containers/TheManagementLayout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  TheContent,
+  TheSidebar,
+  TheFooter,
+  TheHeader
+} from './index'
+import Management from 'src/views/management/Management'
+
+const TheManagementLayout = () => {
+
+  return (
+    <div className="c-app c-default-layout">
+      <TheSidebar/>
+      <div className="c-wrapper">
+        <TheHeader/>
+        <div className="c-body">
+          <Management />
+        </div>
+        <TheFooter/>
+      </div>
+    </div>
+  )
+}
+
+export default TheManagementLayout
