6eac11e4007ede6926662e25bf76a0b0ebabdca0
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2020-08-28 01:31:20 +0000

    added categories view

---
+++ src/views/categories/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CContainer, CCard } from '@coreui/react'
+
+export default class Categories extends React.Component{
+       render(){
+           return(
+                <div className="container">
+                <h1>This is the categories page</h1>
+                </div>
+           )
+       }
+}
