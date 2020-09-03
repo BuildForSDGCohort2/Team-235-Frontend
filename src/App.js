d715f247f5e9b51d312d2d7343e5cdfb4e55dd1d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2020-09-03 15:42:29 +0000

    update user management module

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const Login = React.lazy(() => import('./views/pages/login/Login'));
 const Register = React.lazy(() => import('./views/pages/register/Register'));
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-const AddNewPage = React.lazy(() => import('./views/management/AddNewPage'));
+ 
  
  
 
