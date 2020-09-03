2fdf3aa851ff96fa0a8bed40515d14402cdaba71
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2020-09-03 19:16:02 +0000

    modified user management module

--- src/views/management/Management.js
+++ src/views/management/Management.js
@@ -2,50 +2,47 @@ import React from 'react'
 import  {useState}  from 'react';
 import {Link} from 'react-router-dom';
  
-import {CButton,CCard, CCardBody, CCardHeader,CDataTable,CBadge,CCollapse, CContainer} from '@coreui/react'
+import 
+    {CButton,
+    CCard,
+    CDropdown,
+    CCardHeader,
+    CDataTable,
+    CBadge,
+    CDropdownItem,
+    CDropdownMenu,
+    CDropdownToggle,
+    CContainer} from '@coreui/react'
  
 const TheUserManagement = () => {
   const [details, setDetails] = useState([])
   
     const usersData = [
-        {id: 0, name: 'John Doe', registered: '2018/01/01', role: 'Guest', status: 'Pending'},
-        {id: 1, name: 'Samppa Nori', registered: '2018/01/01', role: 'Member', status: 'Active'},
-        {id: 2, name: 'Estavan Lykos', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
-        {id: 3, name: 'Chetan Mohamed', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
-        {id: 4, name: 'Derick Maximinus', registered: '2018/03/01', role: 'Member', status: 'Pending'},
-        {id: 5, name: 'Friderik Dávid', registered: '2018/01/21', role: 'Staff', status: 'Active'},
-        {id: 6, name: 'Yiorgos Avraamu', registered: '2018/01/01', role: 'Member', status: 'Active'},
-        {id: 7, name: 'Avram Tarasios', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+        {id: 0, name: 'John Doe', email: 'kwakuboafo@gmail.com', phone: '0543243676', status: 'Pending'},
+        {id: 1, name: 'Samppa Nori', email: 'kwakuboafo@gmail.com', phone: '0564438556', status: 'Active'},
+        {id: 2, name: 'Estavan Lykos', email: 'serwahasamoah@gmail.com', phone: '0278834566', status: 'Banned'},
+        {id: 3, name: 'Chetan Mohamed', email: 'serwahasamoah@gmail.com', phone: '0234456782', status: 'Inactive'},
+        {id: 4, name: 'Derick Maximinus', email: 'amakuma@gmail.com', phone: '0563325647', status: 'Pending'},
+        {id: 5, name: 'Friderik Dávid', email: 'sarpongemmanuel@gmail.com', phone: '0278834566', status: 'Active'},
+        {id: 6, name: 'Yiorgos Avraamu', email: 'kwakuboafo@gmail.com', phone: '0563325647', status: 'Active'},
+        {id: 7, name: 'Avram Tarasios', email: 'serwahasamoah@gmail.com', phone: '0278834566', status: 'Banned'},
+         {id: 0, name: 'John Doe', email: 'kwakuboafo@gmail.com', phone: '0543243676', status: 'Pending'},
+        {id: 1, name: 'Samppa Nori', email: 'kwakuboafo@gmail.com', phone: '0564438556', status: 'Active'},
+        {id: 2, name: 'Estavan Lykos', email: 'serwahasamoah@gmail.com', phone: '0278834566', status: 'Banned'},
+        {id: 3, name: 'Chetan Mohamed', email: 'serwahasamoah@gmail.com', phone: '0234456782', status: 'Inactive'},
+        {id: 4, name: 'Derick Maximinus', email: 'amakuma@gmail.com', phone: '0563325647', status: 'Pending'},
+        {id: 5, name: 'Friderik Dávid', email: 'sarpongemmanuel@gmail.com', phone: '0278834566', status: 'Active'},
+        {id: 6, name: 'Yiorgos Avraamu', email: 'kwakuboafo@gmail.com', phone: '0563325647', status: 'Active'},
+        {id: 7, name: 'Avram Tarasios', email: 'serwahasamoah@gmail.com', phone: '0278834566', status: 'Banned'}
         
       ]
     
-    
-    //   const [items, setItems] = useState(usersData)
-    
-      const toggleDetails = (index) => {
-        const position = details.indexOf(index)
-        let newDetails = details.slice()
-        if (position !== -1) {
-          newDetails.splice(position, 1)
-        } else {
-          newDetails = [...details, index]
-        }
-        setDetails(newDetails)
-      }
-    
-    
       const fields = [
-        { key: 'name', _style: { width: '40%'} },
-        'registered',
-        { key: 'role', _style: { width: '20%'} },
+        { key: 'name', _style: { width: '25%'} },
+        {key:'email'},
+        { key: 'phone', _style: { width: '20%'} },
         { key: 'status', _style: { width: '20%'} },
-        {
-          key: 'show_details',
-          label: '',
-          _style: { width: '1%' },
-          sorter: false,
-          filter: false
-        }
+        {key: 'options', _style:{width: '20%'}}
       ]
     
       const getBadge = (status)=>{
@@ -60,7 +57,6 @@ const TheUserManagement = () => {
    
     return (
         <CContainer fluid>
-
         {/**start of card background */}
         <CCard className="cards" style={{borderRadius:"10px", padding:"30px", marginTop:"-20px"}}>
            <CCardHeader>
@@ -88,43 +84,22 @@ const TheUserManagement = () => {
                         </CBadge>
                         </td>
                     ),
-                        'show_details':
-                        //handle show details
-                        (item, index)=>{
-                            return (
-                            <td className="py-2">
-                                <CButton
-                                color="primary"
-                                variant="outline"
-                                shape="square"
-                                size="sm"
-                                onClick={()=>{toggleDetails(index)}}
-                                >
-                  {details.includes(index) ? 'Hide' : 'Show'}
-                </CButton>
+                   'options':
+                    //handle show details
+                    (item, index)=>{
+                        return (
+                        <td className="py-2">
+                      <CDropdown>
+                          <CDropdownToggle>more</CDropdownToggle>
+                              <CDropdownMenu>
+                                <CDropdownItem>Edit</CDropdownItem>
+                                <CDropdownItem>Delete</CDropdownItem>
+                              <CDropdownItem>Assign roles</CDropdownItem>
+                        </CDropdownMenu>
+                     </CDropdown>
               </td>
               )
-              },
-                'details':
-                //show the details of added user
-                    (item, index)=>{
-              return (
-              <CCollapse show={details.includes(index)}>
-                <CCardBody>
-                  <h4>
-                    {item.username}
-                  </h4>
-                  <p className="text-muted">User since: {item.registered}</p>
-                  <CButton size="sm" color="info">
-                    User Settings
-                  </CButton>
-                  <CButton size="sm" color="danger" className="ml-1">
-                    Delete
-                  </CButton>
-                </CCardBody>
-              </CCollapse>
-            )
-          }
+              }
       }}
     />
      </CCard>
